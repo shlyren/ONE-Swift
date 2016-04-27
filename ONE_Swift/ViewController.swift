3b03753456503dec75e0e5fb64f178926ad641bd
@shlyren <shlyren@163.com>
2016-04-27 18:24:27 +0800

    chushihu

---
+++ ONE_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ONE_Swift
+//
+//  Created by 任玉祥 on 16/4/27.
+//  Copyright © 2016年 任玉祥. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+use_frameworks!
+
+target 'ONE_Swift' do
+pod 'AFNetworking'
+pod 'SDWebImage'
+pod 'SVProgressHUD'
+pod 'MJRefresh'
+pod 'MJExtension'
+
+end
+
